16b05d23f5aed4dd6ba0b4c3def381c8869a9b90
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-02-26 23:06:59 +0700

    View Razor

---
+++ SP25_NET1720_RPR231_ASM1_SE171117_HuynhThienNhan/SCBS.MVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SCBS.MVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
